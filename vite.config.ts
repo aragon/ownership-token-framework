c7f92fd115a629480a49d066ace14f2ce877e789
KD <whomst@kevind.xyz>
2026-01-12 17:27:42 +0100

    chore: test log v3

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,29 @@
-import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 import { nitro } from 'nitro/vite'
+import { defineConfig, loadEnv } from "vite"
 
-const config = defineConfig({
-  plugins: [
-    devtools(),
-    nitro(),
-    // this is the plugin that enables path aliases
-    viteTsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
-    tailwindcss(),
-    tanstackStart(),
-    viteReact(),
-  ],
+const config = defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "")
+  const tokenSymbolEnv =
+    env.VITE_TOKEN_SYMBOL ?? env.VERCEL_GIT_COMMIT_REF ?? ""
+
+  return {
+    define: {
+      "import.meta.env.VITE_TOKEN_SYMBOL": JSON.stringify(tokenSymbolEnv),
+    },
+    plugins: [
+      devtools(),
+      nitro(),
+      viteTsConfigPaths({ projects: ["./tsconfig.json"] }),
+      tailwindcss(),
+      tanstackStart(),
+      viteReact(),
+    ],
+  }
 })
 
 console.log("VITE_TOKEN_SYMBOL:", process.env.VITE_TOKEN_SYMBOL)
