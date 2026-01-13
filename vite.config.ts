cb02f843e11d81bb9f040152f316ebc7933f00f0
KD <whomst@kevind.xyz>
2026-01-13 10:38:26 +0100

    fix: build error

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
-<<<<<<< HEAD
-=======
 import { defineConfig, loadEnv } from 'vite'
->>>>>>> 1dd7c8a155a90ecfc8bcc8a8876433211b23293e
 import { devtools } from '@tanstack/devtools-vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 import { nitro } from 'nitro/vite'
-import { defineConfig, loadEnv } from "vite"
 
 const config = defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
